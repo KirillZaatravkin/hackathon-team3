8d2ffb4ea56a7eb389ee6a6cc2b7784fbbce8052
zatravkin-ko <zatravkin-ko@itrev.ru>
2024-01-25 14:31:12 +0300

    init commit

---
+++ src/main/java/ru/team3/HackathonTeam3Application.java
@@ -0,0 +1,13 @@
+package ru.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonTeam3Application.class, args);
+    }
+
+}
